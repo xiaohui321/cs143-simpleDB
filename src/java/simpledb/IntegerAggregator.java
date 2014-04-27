22f31e3224bc39a96d91865952969b80a70a4477
Xiaohui Zhou <zhouxiaohui93321@gmail.com>
2014-04-26 19:21:25 -0700

    aggregators implemented

--- src/java/simpledb/IntegerAggregator.java
+++ src/java/simpledb/IntegerAggregator.java
@@ -1,5 +1,11 @@
 package simpledb;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
 /**
  * Knows how to compute some aggregate over a set of IntFields.
  */
@@ -22,8 +28,43 @@ public class IntegerAggregator implements Aggregator {
      *            the aggregation operator
      */
 
-    public IntegerAggregator(int gbfield, Type gbfieldtype, int afield, Op what) {
-        // some code goes here
+    private final int groupByFieldInt, aggregateFieldInt;
+    private String aggregateFieldName, groupByFieldName;
+    private final Type groupByFieldType;
+    private final Op operator;
+    private final Map<Field, Field> aggregateMap;
+    private final Map<Field, Average> averageMap;
+
+    private class Average {
+	private int sum, count;
+
+	public Average() {
+	    sum = 0;
+	    count = 0;
+	}
+
+	public int getAverage() {
+	    return sum / count;
+	}
+
+	public void addNewItem(final int value) {
+	    sum += value;
+	    count++;
+	}
+    }
+
+    public IntegerAggregator(final int gbfield, final Type gbfieldtype,
+	    final int afield, final Op what) {
+	aggregateFieldInt = afield;
+	aggregateFieldName = "";
+	aggregateMap = new HashMap<Field, Field>();
+
+	groupByFieldInt = gbfield;
+	groupByFieldName = "";
+	groupByFieldType = gbfieldtype;
+
+	operator = what;
+	averageMap = new HashMap<Field, Average>();
     }
 
     /**
@@ -33,8 +74,66 @@ public class IntegerAggregator implements Aggregator {
      * @param tup
      *            the Tuple containing an aggregate field and a group-by field
      */
-    public void mergeTupleIntoGroup(Tuple tup) {
-        // some code goes here
+    @Override
+    public void mergeTupleIntoGroup(final Tuple tup) {
+	Field keyField;
+	aggregateFieldName = tup.getTupleDesc().getFieldName(aggregateFieldInt);
+	if (groupByFieldInt == NO_GROUPING)
+	    keyField = new IntField(NO_GROUPING);
+	else {
+	    keyField = tup.getField(groupByFieldInt);
+	    groupByFieldName = tup.getTupleDesc().getFieldName(groupByFieldInt);
+	}
+	IntField valueField = (IntField) aggregateMap.get(keyField);
+	IntField tupleCorrespondingValueField = (IntField) tup
+	        .getField(aggregateFieldInt);
+	int newValue = 0;
+	switch (operator) {
+	    case AVG:
+		if (valueField == null) {
+		    Average newAverage = new Average();
+		    newValue = tupleCorrespondingValueField.getValue();
+		    newAverage.addNewItem(newValue);
+		    averageMap.put(keyField, newAverage);
+		} else {
+		    Average oldAverage = averageMap.get(keyField);
+		    oldAverage.addNewItem(tupleCorrespondingValueField
+			    .getValue());
+		    averageMap.put(keyField, oldAverage);
+		    newValue = oldAverage.getAverage();
+		}
+		break;
+	    case COUNT:
+		newValue = 1;
+		if (valueField != null)
+		    newValue += valueField.getValue();
+		break;
+	    case MAX:
+		newValue = tupleCorrespondingValueField.getValue();
+		if (valueField != null && valueField.getValue() > newValue)
+		    newValue = valueField.getValue();
+		break;
+	    case MIN:
+		newValue = tupleCorrespondingValueField.getValue();
+		if (valueField != null && valueField.getValue() < newValue)
+		    newValue = valueField.getValue();
+		break;
+	    case SC_AVG:
+		throw new UnsupportedOperationException("unimplemented");
+	    case SUM:
+		newValue = tupleCorrespondingValueField.getValue();
+		if (valueField != null)
+		    newValue += valueField.getValue();
+		break;
+	    case SUM_COUNT:
+		throw new UnsupportedOperationException("unimplemented");
+	    default:
+		// we should never get there
+		throw new UnsupportedOperationException("Unknown operator");
+
+	}
+	IntField newValueField = new IntField(newValue);
+	aggregateMap.put(keyField, newValueField);
     }
 
     /**
@@ -45,10 +144,32 @@ public class IntegerAggregator implements Aggregator {
      *         aggregateVal is determined by the type of aggregate specified in
      *         the constructor.
      */
+    @Override
     public DbIterator iterator() {
-        // some code goes here
-        throw new
-        UnsupportedOperationException("please implement me for lab2");
+	if (groupByFieldInt == NO_GROUPING) {
+	    Type[] types = new Type[] { Type.INT_TYPE };
+	    String[] strings = new String[] { aggregateFieldName };
+	    TupleDesc tupleDesc = new TupleDesc(types, strings);
+	    Tuple tuple = new Tuple(tupleDesc);
+	    tuple.setField(0, aggregateMap.get(new IntField(NO_GROUPING)));
+	    List<Tuple> tupleList = new ArrayList<Tuple>();
+	    tupleList.add(tuple);
+	    return new TupleIterator(tupleDesc, tupleList);
+	} else {
+	    Type[] types = new Type[] { groupByFieldType, Type.INT_TYPE };
+	    String[] strings = new String[] { groupByFieldName,
+		    aggregateFieldName };
+	    TupleDesc tupleDesc = new TupleDesc(types, strings);
+	    List<Tuple> tupleList = new ArrayList<Tuple>();
+	    Set<Field> keySet = aggregateMap.keySet();
+	    for (Field i : keySet) {
+		Tuple tuple = new Tuple(tupleDesc);
+		tuple.setField(0, i);
+		tuple.setField(1, aggregateMap.get(i));
+		tupleList.add(tuple);
+	    }
+	    return new TupleIterator(tupleDesc, tupleList);
+	}
     }
 
 }
--- src/java/simpledb/StringAggregator.java
+++ src/java/simpledb/StringAggregator.java
@@ -1,5 +1,11 @@
 package simpledb;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
 /**
  * Knows how to compute some aggregate over a set of StringFields.
  */
@@ -7,38 +13,102 @@ public class StringAggregator implements Aggregator {
 
     private static final long serialVersionUID = 1L;
 
+    private final int groupByFieldInt, aggregateFieldInt;
+    private String aggregateFieldName, groupByFieldName;
+    private final Type groupByFieldType;
+    private final Map<Field, Field> aggregateMap;
+
     /**
      * Aggregate constructor
-     * @param gbfield the 0-based index of the group-by field in the tuple, or NO_GROUPING if there is no grouping
-     * @param gbfieldtype the type of the group by field (e.g., Type.INT_TYPE), or null if there is no grouping
-     * @param afield the 0-based index of the aggregate field in the tuple
-     * @param what aggregation operator to use -- only supports COUNT
-     * @throws IllegalArgumentException if what != COUNT
+     * 
+     * @param gbfield
+     *            the 0-based index of the group-by field in the tuple, or
+     *            NO_GROUPING if there is no grouping
+     * @param gbfieldtype
+     *            the type of the group by field (e.g., Type.INT_TYPE), or null
+     *            if there is no grouping
+     * @param afield
+     *            the 0-based index of the aggregate field in the tuple
+     * @param what
+     *            aggregation operator to use -- only supports COUNT
+     * @throws IllegalArgumentException
+     *             if what != COUNT
      */
+    public StringAggregator(final int gbfield, final Type gbfieldtype,
+	    final int afield, final Op what) {
+	if (!what.equals(Op.COUNT))
+	    throw new IllegalArgumentException("The Operator must be COUNT");
 
-    public StringAggregator(int gbfield, Type gbfieldtype, int afield, Op what) {
-        // some code goes here
+	aggregateFieldInt = afield;
+	aggregateFieldName = "";
+	aggregateMap = new HashMap<Field, Field>();
+
+	groupByFieldInt = gbfield;
+	groupByFieldName = "";
+	groupByFieldType = gbfieldtype;
     }
 
     /**
-     * Merge a new tuple into the aggregate, grouping as indicated in the constructor
-     * @param tup the Tuple containing an aggregate field and a group-by field
+     * Merge a new tuple into the aggregate, grouping as indicated in the
+     * constructor
+     * 
+     * @param tup
+     *            the Tuple containing an aggregate field and a group-by field
      */
-    public void mergeTupleIntoGroup(Tuple tup) {
-        // some code goes here
+    @Override
+    public void mergeTupleIntoGroup(final Tuple tup) {
+	Field keyField;
+	if (groupByFieldInt == NO_GROUPING)
+	    keyField = new IntField(NO_GROUPING);
+	else {
+	    keyField = tup.getField(groupByFieldInt);
+	    groupByFieldName = tup.getTupleDesc().getFieldName(groupByFieldInt);
+	}
+	aggregateFieldName = tup.getTupleDesc().getFieldName(aggregateFieldInt);
+	IntField valueField = (IntField) aggregateMap.get(keyField);
+
+	int newValue = 1;
+	if (valueField != null)
+	    newValue += valueField.getValue();
+
+	IntField newValueField = new IntField(newValue);
+	aggregateMap.put(keyField, newValueField);
     }
 
     /**
      * Create a DbIterator over group aggregate results.
-     *
-     * @return a DbIterator whose tuples are the pair (groupVal,
-     *   aggregateVal) if using group, or a single (aggregateVal) if no
-     *   grouping. The aggregateVal is determined by the type of
-     *   aggregate specified in the constructor.
+     * 
+     * @return a DbIterator whose tuples are the pair (groupVal, aggregateVal)
+     *         if using group, or a single (aggregateVal) if no grouping. The
+     *         aggregateVal is determined by the type of aggregate specified in
+     *         the constructor.
      */
+    @Override
     public DbIterator iterator() {
-        // some code goes here
-        throw new UnsupportedOperationException("please implement me for lab2");
+	if (groupByFieldInt == NO_GROUPING) {
+	    Type[] types = new Type[] { Type.INT_TYPE };
+	    String[] strings = new String[] { aggregateFieldName };
+	    TupleDesc tupleDesc = new TupleDesc(types, strings);
+	    Tuple tuple = new Tuple(tupleDesc);
+	    tuple.setField(0, aggregateMap.get(new IntField(NO_GROUPING)));
+	    List<Tuple> tupleList = new ArrayList<Tuple>();
+	    tupleList.add(tuple);
+	    return new TupleIterator(tupleDesc, tupleList);
+	} else {
+	    Type[] types = new Type[] { groupByFieldType, Type.INT_TYPE };
+	    String[] strings = new String[] { groupByFieldName,
+		    aggregateFieldName };
+	    TupleDesc tupleDesc = new TupleDesc(types, strings);
+	    List<Tuple> tupleList = new ArrayList<Tuple>();
+	    Set<Field> keySet = aggregateMap.keySet();
+	    for (Field i : keySet) {
+		Tuple tuple = new Tuple(tupleDesc);
+		tuple.setField(0, i);
+		tuple.setField(1, aggregateMap.get(i));
+		tupleList.add(tuple);
+	    }
+	    return new TupleIterator(tupleDesc, tupleList);
+	}
     }
 
 }
